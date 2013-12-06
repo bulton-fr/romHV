30a50dd0ae9507f072fc98ef8cb9f28fa5317563
bulton-gtw <maxou.9@gmail.com>
2013-12-06 15:37:07 +0100

    Ajout des controleurs et vues du header et du footer

---
+++ controlers/footer.php
@@ -0,0 +1,4 @@
+<?php
+$TPL = new \BFW_Tpl\Classes\Template('footer.html');
+$TPL->End();
+?>
---
+++ controlers/header.php
@@ -0,0 +1,20 @@
+<?php
+$TPL = new BFW_Tpl\Classes\Template('header.html');
+$TPL->AddGeneralVars(array('path' => $path));
+
+if(isset($Params_head) && is_array($Params_head))
+{
+	if(isset($Params_head['css']) && is_array($Params_head['css']))
+	{
+		foreach($Params_head['css'] as $link) {$TPL->AddBlock('css', array('link' => $link));}
+		$TPL->EndBlock();
+	}
+	
+	if(isset($Params_head['js']) && is_array($Params_head['js']))
+	{
+		foreach($Params_head['js'] as $link) {$TPL->AddBlock('js', array('link' => $link));}
+		$TPL->EndBlock();
+	}
+}
+
+$TPL->End();
