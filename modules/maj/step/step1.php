e07c17c68d67c4155e6f4c00c85148d3b9d11525
bulton-gtw <maxou.9@gmail.com>
2013-12-13 13:04:35 +0100

    Etape 1 de la maj

---
+++ modules/maj/step/step1.php
@@ -0,0 +1,13 @@
+<?php
+//Sauvegarde la base de données actuelle
+
+require(path_modules.'BFW_Sql/config.php');
+$date = new \BFW\CKernel\Date;
+$nameFile = 'dump_'.$date->annee.'-'.$date->mois.'-'.$date->jour.'_'.$date->heure.'-'.$date->minute.'-'.$date->seconde.'.sql.gz';
+
+$cmd  = 'mysqldump --host='.$bd_host.' --user='.$bd_user.' --password='.$bd_pass;
+$cmd .= ' --no-create-db --default-character-set=utf8 --lock-tables=FALSE --tables '.$bd_name;
+$cmd .= ' | gzip > ../sauv_bdd/'.$nameFile;
+
+system($cmd);
+?>
