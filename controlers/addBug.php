89f35eceb99191ad899aa13d39a45375b88724a8
bulton-gtw <maxou.9@gmail.com>
2014-01-24 00:48:03 +0100

    #36 : Ajout de l'envoi d'un mail vers bulton-fr

--- controlers/addBug.php
+++ controlers/addBug.php
@@ -14,6 +14,17 @@ catch(InvalidArgumentException $e) {ErrorView(403, false);}
 try
 {
 	$client->api('issue')->create('bulton-fr', 'romHV', array('title' => $titre, 'body' => $body, 'labels' => array('à valider')));
+    
+    $mail = new PHPMailer\PHPMailer(); // defaults to using php "mail()"
+    
+    $mail->AddReplyTo('bulton.fr@gmail.com', 'Bulton.fr');
+    $mail->SetFrom('bulton.fr@gmail.com', 'Bulton.fr');
+    $mail->AddAddress('bulton.fr@gmail.com', 'Bulton.fr');
+    
+    $mail->Subject = utf8_decode('Nouveau bug reporté sur romHV');
+    $mail->Body = utf8_decode(htmlentities(addslashes($_SESSION['Login']))).' vient de signaler un nouveau bug sur romHV : '.utf8_decode(htmlentities(addslashes($titre)));
+    
+    $mail->Send();
 }
 catch(exception $e) {ErrorView(500, false);}
 ?>
